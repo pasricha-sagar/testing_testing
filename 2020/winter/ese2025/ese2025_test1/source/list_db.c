86021c00cdec6054bd6036feda9743cc7c36fd4d
sagar pasricha <sagar89011@gmail.com>
2020-04-20 20:56:34 -0400

    initialized counter and find node function changed

--- 2020/winter/ese2025/ese2025_test1/source/list_db.c
+++ 2020/winter/ese2025/ese2025_test1/source/list_db.c
@@ -96,6 +96,8 @@ stuLL_t* findLinkedList(stuLL_t *pHEAD, char *targetFamilyName)
 		{
 			// PROBLEM #2: complete the code for this function
 			//	       NOTE: PLEASE COMMIT CHANGES ONCE YOUR DONE THIS FUNCTION
+			pW = pW -> pNext;
+			i =i+1;
 		}
 		printf("\n");
 	}
